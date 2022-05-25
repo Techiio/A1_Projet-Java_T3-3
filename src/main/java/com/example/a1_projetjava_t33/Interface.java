5230ad2beaf474205a75df280c6d345017cc7bab
Benoit <benoit.thomas@eleve.isep.fr>
2022-05-25 17:56:00 +0200

    décompte des points et affichage du vainqueur

--- src/main/java/com/example/a1_projetjava_t33/Interface.java
+++ src/main/java/com/example/a1_projetjava_t33/Interface.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 //Interface de jeu
 public class Interface {
@@ -44,7 +45,7 @@ public class Interface {
             }
         }
 
-        int usedToken = 1;
+        AtomicInteger usedToken = new AtomicInteger(1);
 //--------------------------------Sons ------------------------------------------------
         //Son lors d'un clic
         String soundFile = "clic.mp3";
@@ -126,25 +127,50 @@ public class Interface {
         //Mise en place de la liste de joueurs
 
         VBox leftBorderPane = new VBox();
-
+        //Itération pour le nombre de joueurs
         for(int i = 1; i <= Menu.nbplayer; i++) {
-            Dictionary<String,Integer> nbVictoires = new Hashtable<String,Integer>();
+            Dictionary<String,Integer> nbVictoires = new Hashtable<String,Integer>(); //Dictionnaire pour associé à chaque joueur son nombre de victoires
             final int[] count = {0};
             nbVictoires.put("Joueur "+i, count[0]);
-
+            //Création d'un bouton
             Button btn = new Button("Joueur "+ i + " : " + nbVictoires.get("Joueur "+i));
             Color playerColor = Color.color(Math.random(), Math.random(), Math.random());
             btn.setTextFill(playerColor);
 
             int finalI = i;
+            final Integer[] decompte = {0};
+            final Integer[] decomptebis = {0};
+            AtomicInteger winner = new AtomicInteger();
+            //A chaque fois qu'un joueur gagne une manche: il clique sur un bouton
             btn.setOnAction(event -> {
+                //Ici on incrémente le score du joueur ayant gagné la manche
                 count[0] = count[0] +1;
                 nbVictoires.put("Joueur "+ finalI, count[0]);
                 btn.setText("Joueur "+ finalI + " : " + nbVictoires.get("Joueur "+ finalI));
-                if(usedToken < nbToken){
+                //S'il reste des token la partie continue
+                if(usedToken.get() < nbToken){
                     turnWin.play();
+                    usedToken.set(usedToken.get() + 1);
+                //Sinon on fait le décompte des points et affiche le vainqueur
                 }else{
                     victory.play();
+                    for(int k = 1; k < Menu.nbplayer; k++){
+                        decompte[0] = nbVictoires.get("Joueur "+k);
+                        decomptebis[0] = nbVictoires.get("Joueur "+(k+1));
+                        if (decompte[0] == null){
+                            decompte[0] = 0;
+                        }
+                        if (decomptebis[0] == null){
+                            decomptebis[0] = 0;
+                        }
+                        if(decompte[0] <= decomptebis[0]){
+                            winner.set(k + 1);
+                        }else{
+                            winner.set(k);
+                        }
+                    }
+                    btnTop.setText("VAINQUEUR : Joueur "+ winner);
+                    btnTop.setTextFill(Color.color(1, 0, 0));
                 }
 
             });
