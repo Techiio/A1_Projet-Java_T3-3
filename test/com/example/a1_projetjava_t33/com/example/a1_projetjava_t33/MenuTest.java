85a0dd292cd47ff2d653230999ef61d002c52a80
Sacha-Bleicher <91735814+Sacha-Bleicher@users.noreply.github.com>
2022-06-03 10:51:17 +0200

    Tests unitaire

---
+++ test/com/example/a1_projetjava_t33/com/example/a1_projetjava_t33/MenuTest.java
@@ -0,0 +1,12 @@
+package com.example.a1_projetjava_t33;
+
+import org.junit.jupiter.api.Test;
+
+import static com.example.a1_projetjava_t33.Menu.nbplayer;
+import static org.junit.jupiter.api.Assertions.*;
+public class MenuTest {
+    @Test
+    public void testnbplayers() throws Exception  {
+        assertNull(nbplayer);
+    }
+}
